2e35177f555ca59cda3d302187869b895daef39f
nahuelvazquez88 <77303438+nahuelvazquez88@users.noreply.github.com>
2021-10-27 16:41:56 -0300

    Add files via upload

---
+++ battlefield.js
@@ -0,0 +1,10 @@
+var nombre=prompt("ingrese el nombre");
+
+
+
+var edad=prompt("ingrese su edad");
+
+var telefono=prompt("ingrese su mail ");
+
+var mensajefinal="Nombre: " + nombre+ " .edad:"+ edad+ " .mail:"+ mail
+alert(mensajefinal)
